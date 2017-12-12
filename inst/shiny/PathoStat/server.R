0e5e46f3f0543165f9019d05f9fe5c2a3bb49bcf
jasonzhao0307 <jasonzhao0307@gmail.com>
2017-12-11 23:34:31 -0500

    test

--- inst/shiny/PathoStat/server.R
+++ inst/shiny/PathoStat/server.R
@@ -123,9 +123,8 @@ shinyServer(function(input, output, session) {
         }, "hover") %>%
         # add_axis('x', subdivide = 1,
         #   values = 1:length(colnames(shinyInput$data)),
-        add_axis("x", title = "Test samples", properties = axis_props(title =
-            list(fontSize = 15), labels = list(text = "",
-            fontSize = 10))) %>%
+        add_axis("x", title = "Samples", properties = axis_props(title =
+            list(fontSize = 15))) %>%
         add_axis("y", title = "Relative Abundance (RA)", properties =
             axis_props(title = list(fontSize = 15),
             labels = list(fontSize = 10))) %>% add_legend("fill", title =
