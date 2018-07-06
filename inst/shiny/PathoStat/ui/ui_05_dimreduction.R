f1ab48959a1de797ff5cd82944db6a8c25ba2281
Anthony Federico <anfederico@users.noreply.github.com>
2018-07-06 18:21:52 -0500

    Clean up PCA plots

--- inst/shiny/PathoStat/ui/ui_05_dimreduction.R
+++ inst/shiny/PathoStat/ui/ui_05_dimreduction.R
@@ -1,32 +1,39 @@
-
 tabPanel("Dimension Reduction",
-         sidebarLayout(
-             sidebarPanel(
-                 numericInput('xcol.new', 'Principal Component (x-axis)', 1,
-                              min = 1, max = 50),
-                 numericInput('ycol.new', 'Principal Component (y-axis)', 2,
-                              min = 1, max = 50),
-                 selectizeInput('taxl.pca', 'Taxonomy Level', choices = tax.name,
-                                selected='no rank'),
-                 selectInput("select_pca_color", "Color points by:",
-                             covariates),
-                 selectInput("select_pca_shape", "Shape points by:",
-                             covariates.colorbar),
-                 actionButton("DR_plot", "Plot"),
-                 width=3
-             ),
-             mainPanel(
-                 tabsetPanel(
-                     tabPanel("PCA plot",
-                              plotlyOutput("pca.plotly"),
-                              selectInput("select_pca_data_format", "Select data type", c("read count", "log10 CPM", "RA"))),
-                     tabPanel("PCA variance", DT::dataTableOutput("PCAtable")),
-                     tabPanel("PCoA plot",
-                              plotlyOutput("pcoa.plotly"),
-                              selectInput("pcoa.method", "PCoA method:",
-                                          beta.methods)),
-                     tabPanel("PCoA variance", DT::dataTableOutput("PCoAtable"))
-                 )
-             )
-         )
+  sidebarLayout(
+    sidebarPanel(
+      numericInput('xcol.new', 'Principal Component (x-axis)', 1, min = 1, max = 50),
+      numericInput('ycol.new', 'Principal Component (y-axis)', 2, min = 1, max = 50),
+      selectizeInput('taxl.pca', 'Taxonomy Level', choices = tax.name, selected='no rank'),
+      selectInput("select_pca_color", "Color points by:", covariates),
+      selectInput("select_pca_shape", "Shape points by:", covariates.colorbar),
+      actionButton("DR_plot", "Plot"),
+      width=3
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("PCA",
+          fluidRow(
+            column(9,
+              plotlyOutput("pca.plotly", height="500px"),
+              selectInput("select_pca_data_format", "Select data type", c("read count", "log10 CPM", "RA"))
+            ),
+            column(3,
+              DT::dataTableOutput("PCAtable")
+            )
+          )
+        ),
+        tabPanel("PCoA",
+          fluidRow(
+            column(9,
+              plotlyOutput("pcoa.plotly", height="500px"),
+              selectInput("pcoa.method", "PCoA method:", beta.methods)
+            ),
+            column(3,
+              DT::dataTableOutput("PCoAtable")
+            )
+          )
+        )
+      )
+    )
+  )
 )
