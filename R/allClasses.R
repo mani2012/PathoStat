80217ccf81a6a8031a11cb15190e5fc4a65ade34
jasonzhao0307 <jasonzhao0307@gmail.com>
2018-10-22 21:50:42 -0400

    update bioc

--- R/allClasses.R
+++ R/allClasses.R
@@ -12,8 +12,19 @@
 #' }
 #'
 #' @export pathostat1
-#' @examples 
-#' pathostat1()
+#' @examples
+#' otumat = matrix(sample(1:100, 100, replace = TRUE), nrow = 10, ncol = 10)
+#' rownames(otumat) <- paste0("OTU", 1:nrow(otumat))
+#' colnames(otumat) <- paste0("Sample", 1:ncol(otumat))
+#' taxmat = matrix(sample(letters, 70, replace = TRUE), 
+#' nrow = nrow(otumat), ncol = 7)
+#' rownames(taxmat) <- rownames(otumat)
+#' colnames(taxmat) <- c("Domain", "Phylum", "Class", 
+#' "Order", "Family", "Genus", "Species")
+#' OTU = phyloseq::otu_table(otumat, taxa_are_rows = TRUE)
+#' TAX = phyloseq::tax_table(taxmat)
+#' physeq = phyloseq::phyloseq(OTU, TAX)
+#' pathostat1(physeq)
 #' 
 #' @exportClass PathoStat
 pathostat1 <- setClass(Class="PathoStat",
